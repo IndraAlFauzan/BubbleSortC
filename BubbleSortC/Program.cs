cbed62cf9503e39554440a636c9c83ccc3db68b8
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2022-10-07 16:41:20 +0700

    Add project files.

---
+++ BubbleSortC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BubbleSortC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
